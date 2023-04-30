38e23daceb297abc0642f656cd943a269b071036
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 16:22:43 +0100

    Added builder pattern

--- src/main/java/com/jayblinksLogistics/dto/response/AddOrderResponse.java
+++ src/main/java/com/jayblinksLogistics/dto/response/AddOrderResponse.java
@@ -1,10 +1,12 @@
 package com.jayblinksLogistics.dto.response;
 
 import com.jayblinksLogistics.models.OrderStatus;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 @Data
+@Builder
 public class AddOrderResponse {
     private String message;
     private int statusCode;
