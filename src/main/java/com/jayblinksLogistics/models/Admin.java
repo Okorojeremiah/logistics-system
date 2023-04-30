281df8b1c4c2f332723c8486a7d375cc2c3375a8
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 16:31:26 +0100

    added courierList to admin field

--- src/main/java/com/jayblinksLogistics/models/Admin.java
+++ src/main/java/com/jayblinksLogistics/models/Admin.java
@@ -6,9 +6,14 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Document
 public class Admin extends User{
+    private List<Courier> courierList = new ArrayList<>();
 }
