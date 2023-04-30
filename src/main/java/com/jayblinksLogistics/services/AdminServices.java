0aee2a0509e33130313aa10a92a9016a40a5f036
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 17:07:16 +0100

    updated deleteSenderAccount and deleteCourierAccount functionalities

--- src/main/java/com/jayblinksLogistics/services/AdminServices.java
+++ src/main/java/com/jayblinksLogistics/services/AdminServices.java
@@ -1,8 +1,10 @@
 package com.jayblinksLogistics.services;
 
+import com.jayblinksLogistics.dto.request.CourierRequest;
+import com.jayblinksLogistics.dto.response.CourierResponse;
 import com.jayblinksLogistics.models.Courier;
 import com.jayblinksLogistics.models.Order;
-import com.jayblinksLogistics.models.OrderStatus;
+import com.jayblinksLogistics.models.enums.OrderStatus;
 import com.jayblinksLogistics.models.Sender;
 
 import java.util.List;
@@ -10,8 +12,9 @@ import java.util.List;
 public interface AdminServices {
     Sender findSender(String id);
     Courier findCourier(String id);
-    void deleteSender(String userId);
-    void deleteCourier(String id);
+    void deleteSenderAccount(String userId);
+    CourierResponse assignCourier(CourierRequest courierRequest);
+    void deleteCourierAccount(String id);
     List<Order> viewAllOrders();
     List<Order> findOrdersByCurrentStatus(OrderStatus orderStatus);
 }
