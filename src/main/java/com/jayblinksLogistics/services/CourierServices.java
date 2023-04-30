b12f15ac8a0ab5b7f64c44a1417e4220191690da
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 17:24:40 +0100

    added acceptSenderOrder and checkCourierStatus functionalities

--- src/main/java/com/jayblinksLogistics/services/CourierServices.java
+++ src/main/java/com/jayblinksLogistics/services/CourierServices.java
@@ -1,20 +1,18 @@
 package com.jayblinksLogistics.services;
 
 import com.jayblinksLogistics.dto.request.DeliveryRequest;
+import com.jayblinksLogistics.dto.response.CourierStatusResponse;
 import com.jayblinksLogistics.dto.response.DeliveryResponse;
-import com.jayblinksLogistics.models.Courier;
-import com.jayblinksLogistics.models.Order;
-import com.jayblinksLogistics.models.OrderStatus;
+import com.jayblinksLogistics.models.*;
 
 import java.util.List;
 
 public interface CourierServices {
     DeliveryResponse confirmDelivery(DeliveryRequest deliveryRequest);
+    DeliveryResponse acceptSenderOrder(String senderId);
     Courier findCourier(String id);
-    OrderStatus checkDeliveryStatus(String orderId);
     void deleteCourier(String id);
-
-    List<Order> findOrdersByCurrentStatus(OrderStatus orderStatus);
-
+    void save(Courier courier);
+    CourierStatusResponse checkCourierStatus(String courierId);
 
 }
