630d67f8ee0d3d9b35de4324b8010fb8ec365a8c
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 17:26:49 +0100

    added message and statusCode fields

--- src/main/java/com/jayblinksLogistics/dto/response/DeliveryResponse.java
+++ src/main/java/com/jayblinksLogistics/dto/response/DeliveryResponse.java
@@ -1,9 +1,11 @@
 package com.jayblinksLogistics.dto.response;
 
-import com.jayblinksLogistics.models.OrderStatus;
+import com.jayblinksLogistics.models.enums.OrderStatus;
 import lombok.Data;
 
 @Data
 public class DeliveryResponse {
     private OrderStatus orderStatus;
+    private String message;
+    private int statusCode;
 }
