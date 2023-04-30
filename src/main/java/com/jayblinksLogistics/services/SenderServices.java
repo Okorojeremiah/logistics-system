ddf680b87406407a24669acb9c8c9e01a01a7ecd
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-04-30 17:35:00 +0100

    updated cancelOrder functionality and added checkDeliveryStatus functionality

--- src/main/java/com/jayblinksLogistics/services/SenderServices.java
+++ src/main/java/com/jayblinksLogistics/services/SenderServices.java
@@ -4,15 +4,17 @@ package com.jayblinksLogistics.services;
 import com.jayblinksLogistics.dto.request.AddOrderRequest;
 import com.jayblinksLogistics.dto.response.AddOrderResponse;
 import com.jayblinksLogistics.models.Order;
+import com.jayblinksLogistics.models.enums.OrderStatus;
 import com.jayblinksLogistics.models.Sender;
 
 import java.util.List;
 
 public interface SenderServices {
     List<Order> viewOrderHistory(String senderId);
-    void cancelOrder(String orderId);
+    AddOrderResponse cancelOrder(String orderId);
     AddOrderResponse placeOrder(AddOrderRequest addOrderRequest);
 
+    OrderStatus checkDeliveryStatus(String orderId);
     Sender findSender(String id);
 
     void deleteSender(String id);
